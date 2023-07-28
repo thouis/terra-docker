871433db6e4b0f028f99722c4f1543092f1ac9b5
Thouis (Ray) Jones <thouis@broadinstitute.org>
2023-07-28 12:29:21 -0400

    build for minimal torch

--- terra-jupyter-minimal-gpu-base/build_docker.sh
+++ terra-jupyter-minimal-gpu-base/build_docker.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
+cp -r ../terra-jupyter-base/custom .
+cp -r ../terra-jupyter-base/scripts .
+cp -r ../terra-jupyter-base/jupyter_notebook_config.py .
 
-base="nvcr.io/nvidia/cuda:11.3.1-base-ubuntu20.04"
+gcloud builds submit --timeout=40m --machine-type=N1_HIGHCPU_8 --tag us.gcr.io/landerlab-atacseq-200218/landerlab-torch-base:0.3
 
-cp -r ../terra-jupyter-base/custom ../terra-jupyter-minimal-base/custom
-cp -r ../terra-jupyter-base/scripts ../terra-jupyter-minimal-base/scripts
-cp -r ../terra-jupyter-base/jupyter_notebook_config.py ../terra-jupyter-minimal-base/jupyter_notebook_config.py
+rm -r custom scripts jupyter_notebook_config.py
 
-docker build \
-  -t terra-jupyter-minimal-gpu-base:0.0.1 \
-  -f ../terra-jupyter-minimal-base/Dockerfile \
-  --build-arg BASE_IMAGE=${base} \
-  ../terra-jupyter-minimal-base/
-
-rm -r ../terra-jupyter-minimal-base/custom
-rm -r ../terra-jupyter-minimal-base/scripts
-rm ../terra-jupyter-minimal-base/jupyter_notebook_config.py
